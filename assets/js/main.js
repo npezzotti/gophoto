47008c5e628496ae3e32e16dcfb45650ba327651
npezzotti <nathan.pezzotti@hashicorp.com>
2025-10-15 16:49:42 -0400

    format timestamps in user's local

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,15 @@
 const toastElList = document.querySelectorAll('.toast')
 const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl).show())
+
+
+// Convert all <time> elements with a data-timestamp attribute to localized date strings
+document.querySelectorAll('time[data-timestamp]').forEach(el => {
+  const date = new Date(el.dataset.timestamp);
+  el.textContent = date.toLocaleDateString([], {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+  });
+});
