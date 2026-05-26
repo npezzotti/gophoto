f81ee04833702190f12a05ab9a3c997535fffbfd
npezzotti <nathan.pezzotti@hashicorp.com>
2026-05-26 08:45:14 -0400

    parameterize test

--- internal/web/flash_test.go
+++ internal/web/flash_test.go
@@ -8,28 +8,48 @@ import (
 )
 
 func TestFlash__flash(t *testing.T) {
-	a := application{
-		sessionManager: scs.New(),
+	tcases := []struct {
+		name       string
+		flashLevel flashClass
+	}{
+		{
+			name:       "info flash is stored in session",
+			flashLevel: flashInfo,
+		},
+		{
+			name:       "error flash is stored in session",
+			flashLevel: flashErr,
+		},
 	}
 
-	// Set up a test session context with a test session token
-	ctx, err := a.sessionManager.Load(context.Background(), "test-session-token")
-	if err != nil {
-		t.Fatalf("failed to load session context: %v", err)
-	}
+	for _, tt := range tcases {
+		t.Run(tt.name, func(t *testing.T) {
+			const sessionToken = "test-session-token"
 
-	a.flash(ctx, "hello", flashInfo)
-	val := a.sessionManager.Get(ctx, sessionKeyFlash)
-	flash, ok := val.(Flash)
-	if !ok {
-		t.Fatalf("expected value to be of type \"Flash\", got %T", val)
-	}
+			a := application{
+				sessionManager: scs.New(),
+			}
 
-	expected := Flash{
-		Message: "hello",
-		Level:   flashInfo,
-	}
-	if flash != expected {
-		t.Fatalf("got %v, expected %v", flash, expected)
+			// Set up a test session context with a test session token
+			ctx, err := a.sessionManager.Load(context.Background(), sessionToken)
+			if err != nil {
+				t.Fatalf("failed to load session context: %v", err)
+			}
+
+			a.flash(ctx, "hello", tt.flashLevel)
+			val := a.sessionManager.Get(ctx, sessionKeyFlash)
+			flash, ok := val.(Flash)
+			if !ok {
+				t.Fatalf("expected value to be of type \"Flash\", got %T", val)
+			}
+
+			expected := Flash{
+				Message: "hello",
+				Level:   tt.flashLevel,
+			}
+			if flash != expected {
+				t.Fatalf("got %v, expected %v", flash, expected)
+			}
+		})
 	}
 }
